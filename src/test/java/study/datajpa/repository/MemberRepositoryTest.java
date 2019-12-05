25c37c3dd36843a68580e3b395f3e47f6d9fe221
8c6t <nkw8620@gmail.com>
2019-12-06 01:34:43 +0900

    7.3 Projections

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -381,5 +381,29 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("m1");
     }
 
+    @Test
+    public void projections() {
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        List<NestedClosedProjections> result = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class);
+
+        result.stream().forEach(usernameOnly -> {
+            String username = usernameOnly.getUsername();
+            System.out.println("usernameOnly = " + username);
+
+            String teamName = usernameOnly.getTeam().getName();
+            System.out.println("usernameOnly = " + teamName);
+        });
+    }
 
 }
